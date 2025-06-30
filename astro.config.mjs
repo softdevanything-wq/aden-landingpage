0dfe9db97f876adef9b088047ed636422119bd6e
Mario Reder <mario.reder@pm.me>
2025-06-30 16:29:42 +0200

    pages deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,8 @@ import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 
 export default defineConfig({
+  site: "http://github-dev.orderly.network",
+  base: "/aden-landingpage",
   integrations: [tailwind()],
   output: "static",
   build: {
