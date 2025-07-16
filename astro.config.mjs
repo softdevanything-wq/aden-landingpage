28fd1d5cf9462813c7c5a1e7678219efcd8da841
Mario Reder <mario.reder@pm.me>
2025-07-16 22:26:39 +0200

    change base path

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,7 @@ import tailwind from "@astrojs/tailwind";
 
 export default defineConfig({
   site: "https://aden.io",
-  base: "/v1",
+  base: "/",
   integrations: [tailwind()],
   output: "static",
   build: {
